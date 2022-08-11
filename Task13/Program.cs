1a146ecbd5fe0005812a72d1e8bcabecc0ed2016
Igor <dedul_igor@mail.ru>
2022-08-11 15:00:13 +0300

    complite Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -9,11 +9,21 @@
 Console.WriteLine("Добрый день! Данная программа показывает третью цифру числа.");
 Console.WriteLine("Введите число:");
 int num = Convert.ToInt32(Console.ReadLine());
+int i = 1; //Счётчик определения порядка
+int remain = num;
 
 if (num >=100 || num <=-100)
 {
-    num = num % 100;
-    num = num / 10;
+    do
+    {
+        remain = remain / 10;
+        i = i * 10;
+    }
+    while (remain !=0);
+
+    i = i / 1000;
+    num = num / i;
+    num = num % 10;
     Console.WriteLine($"Третья цифра {num}");
 }
 else
