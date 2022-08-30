75602584c79c71a8b5e88249aeb9f1b819dd669f
Igor <dedul_igor@mail.ru>
2022-08-30 22:30:38 +0300

    Complite Task 52

---
+++ Task52/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("Добрый день! Данная программа находит среднее арифметическое элементов в каждом столбце.");
+Console.WriteLine("");
+Console.WriteLine("Сколько строк должно быть в матрице?");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сколько столбцов должно быть в матрице?");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+void CorrectInput()
+{
+    if (col <= 0 || row <= 0)
+    {
+        Console.WriteLine("Вы ввели некорректный размер матрицы.");
+    }
+}
+
+CorrectInput();
+
+int[,] NewMatrix()
+{
+    int[,] matrix = new int [row,col];
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<col; j++)
+        {
+        matrix[i,j] = new Random().Next(0,100);
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = NewMatrix();
+
+void MatrixPrint()
+{
+    for (int i=0; i<row; i++)
+    {
+        Console.WriteLine("");
+        Console.Write("[");
+        for (int j=0; j<col; j++)
+        {
+        Console.Write($"\t {matrix[i,j]} \t");
+        }
+        Console.Write("]");
+    }
+}
+MatrixPrint();
+Console.WriteLine("");
+Console.WriteLine("Среднее арефметическое столбцов соответственно:");
+
+void ArithmeticMeanCol()
+{
+    double arithmeticMean = default;
+    for (int j=0; j<col; j++)
+    {
+        arithmeticMean = 0;
+        for (int i=0; i<row; i++)
+        {
+            arithmeticMean = arithmeticMean + matrix[i,j];
+        }
+        Console.Write($"\t {arithmeticMean/row} \t");
+    }
+}
+
+ArithmeticMeanCol();
+Console.WriteLine("");
